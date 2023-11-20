3991297a6e7a0621befd44d73e62a04219d49fb5
19992018 <margretmauno16@gmail.com>
2023-11-20 12:02:29 +0300

    added modal

---
+++ public/index.js
@@ -0,0 +1,11 @@
+// const btn = document.getElementsByClassName("to-modal");
+
+// for (let i=0; i < btn.length; i++ ){
+//     btn[i].onclick = () => {
+//         console.log("Hello Mark");
+//     }
+// }
+
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
