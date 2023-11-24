354919c63bf099b71a205e0997ccb323d3785953
19992018 <margretmauno16@gmail.com>
2023-11-24 14:23:50 +0300

    Styled pages with grid layout

--- public/index.js
+++ public/index.js
@@ -15,4 +15,16 @@ import { Input, initMDB } from "mdb-ui-kit";
 
 initMDB({ Input });
 
- 
+/* 
+Day 5
+Worked on the footers.
+Fixed the odd look it had on most pages.
+Take it up from results page.
+
+Day 6:
+Session 1: 8 to 10am
+Styled Home Contact and About Page
+Home: only sizing images remaining
+
+Research on making grid system responsive. Continue with research after the break.
+*/
